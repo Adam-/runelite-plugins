5f772b37b02f092b0c2d1e60ad1c7e117f577a86
Dyldozer <whitedylan7@gmail.com>
2019-12-20 11:07:39 -0600

    Fixed knives

--- src/main/java/com/suppliestracker/ItemType.java
+++ src/main/java/com/suppliestracker/ItemType.java
@@ -66,7 +66,8 @@ public enum ItemType
 		if (item.getName().toLowerCase().contains("bolt") || item.getName().toLowerCase().contains("dart")
 			|| item.getName().toLowerCase().contains(" arrow") || item.getName().toLowerCase().contains("javelin")
 			|| item.getName().toLowerCase().contains("knive") || item.getName().toLowerCase().contains("throwing")
-			|| item.getName().toLowerCase().contains("zulrah's scale") || item.getName().toLowerCase().contains("cannonball"))
+			|| item.getName().toLowerCase().contains("zulrah's scale") || item.getName().toLowerCase().contains("cannonball")
+			|| item.getName().toLowerCase().contains("knife"))
 		{
 			return ItemType.AMMO;
 		}
