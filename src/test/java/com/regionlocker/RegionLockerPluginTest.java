549cefd8e3f7dafdf21adc5c449271053165e5eb
Slay to Stay <slaytostay@tuta.io>
2019-12-22 15:25:23 +0100

    Plugin initialization

--- src/test/java/com/regionlocker/RegionLockerPluginTest.java
+++ src/test/java/com/regionlocker/RegionLockerPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.regionlocker;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class RegionLockerPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(RegionLockerPlugin.class);
 		RuneLite.main(args);
 	}
 }
