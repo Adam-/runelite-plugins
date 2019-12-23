4e98a38d83b61137f4b2eeb17b99db0c76a150ae
Slay to Stay <slaytostay@tuta.io>
2019-12-23 13:15:26 +0100

    Add GPU plugin for custom locked region shaders Add goaltracker plugin

--- src/test/java/com/regionlocker/RegionLockerPluginTest.java
+++ src/test/java/com/regionlocker/RegionLockerPluginTest.java
@@ -1,5 +1,7 @@
 package com.regionlocker;
 
+import com.goaltracker.GoalTrackerPlugin;
+import com.gpu.RegionGpuPlugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
@@ -7,7 +9,7 @@ public class RegionLockerPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(RegionLockerPlugin.class);
+		ExternalPluginManager.loadBuiltin(RegionLockerPlugin.class, RegionGpuPlugin.class, GoalTrackerPlugin.class);
 		RuneLite.main(args);
 	}
 }
