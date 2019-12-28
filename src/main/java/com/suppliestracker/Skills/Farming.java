4863dcd98698de7250397982697a7610ef5dd8ca
Dylan White <Whitedylan7@gmail.com>
2019-12-28 03:24:01 -0600

    separated bucket and plant check for quick clicker

--- src/main/java/com/suppliestracker/Skills/Farming.java
+++ src/main/java/com/suppliestracker/Skills/Farming.java
@@ -13,6 +13,7 @@ public class Farming
 	private ItemManager itemManager;
 	private int plantId = 0;
 	private int compostId = 0;
+	private int bucketId = 0;
 	private final int[] ALLOTMENT_SEEDS = new int[]{POTATO_SEED, ONION_SEED, CABBAGE_SEED, TOMATO_SEED, SWEETCORN_SEED, STRAWBERRY_SEED, WATERMELON_SEED, SNAPE_GRASS_SEED};
 
 	/***	Will be switching all to onMenuOptionClicked similar to how
@@ -24,43 +25,60 @@ public class Farming
 		this.itemManager = itemManager;
 	}
 
-	public void OnChat(String message)
-	{
-			if (plantId <= 0)
-			{
-				return;
-			}
-			String name = itemManager.getItemComposition(plantId).getName().toLowerCase();
-			if ( name.contains(" seed"))
-			{
-				for (int seedId: ALLOTMENT_SEEDS)
-				{
-					if (plantId == seedId)
-					{
-						plugin.buildEntries(plantId, 3);
-						return;
-					}
-				}
-				plugin.buildEntries(plantId);
-			}
-			else if (name.contains(" compost") || name.contains("plant cure") || name.contains(" sapling"))
-			{
-				System.out.println(plantId);
-				if (plantId == BOTTOMLESS_COMPOST_BUCKET || plantId == BOTTOMLESS_COMPOST_BUCKET_22997)
-				{
-					plugin.buildEntries(compostId);
-				}
-				else
-				{
-					plugin.buildEntries(plantId);
-				}
-			}
-	}
+    public void OnChatPlant(String message)
+    {
+        if (plantId <= 0)
+        {
+            return;
+        }
 
-	public void setPlantId(int plantId)
-	{
-		this.plantId = plantId;
-	}
+        String name = itemManager.getItemComposition(plantId).getName().toLowerCase();
+
+        if ( name.contains(" seed") || name.contains(" sapling"))
+        {
+            for (int seedId: ALLOTMENT_SEEDS)
+            {
+                if (plantId == seedId)
+                {
+                    plugin.buildEntries(plantId, 3);
+                    return;
+                }
+            }
+            plugin.buildEntries(plantId);
+        }
+    }
+
+    public void OnChatTreat(String message)
+    {
+        if (bucketId <= 0)
+        {
+            return;
+        }
+
+        String name = itemManager.getItemComposition(bucketId).getName().toLowerCase();
+
+        if (name.contains(" compost") || name.contains("plant cure"))
+        {
+            if (bucketId == BOTTOMLESS_COMPOST_BUCKET || bucketId == BOTTOMLESS_COMPOST_BUCKET_22997)
+            {
+                plugin.buildEntries(compostId);
+            }
+            else
+            {
+                plugin.buildEntries(bucketId);
+            }
+        }
+    }
+
+    public void setPlantId(int plantId)
+    {
+        this.plantId = plantId;
+    }
+
+    public void setBucketId(int bucketId)
+    {
+        this.bucketId = bucketId;
+    }
 
 	public void setEndlessBucket(String message)
 	{
