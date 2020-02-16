6301fca5a1707807ecd86810b1aeb8f18db85de1
Dyldozer <whitedylan7@gmail.com>
2020-02-16 01:12:30 -0600

    fixed compost bug

--- src/main/java/com/suppliestracker/Skills/Farming.java
+++ src/main/java/com/suppliestracker/Skills/Farming.java
@@ -57,7 +57,7 @@ public class Farming
 
         String name = itemManager.getItemComposition(bucketId).getName().toLowerCase();
 
-        if (name.contains(" compost") || name.contains("plant cure"))
+        if (name.contains("compost") || name.contains("plant cure"))
         {
             if (bucketId == BOTTOMLESS_COMPOST_BUCKET || bucketId == BOTTOMLESS_COMPOST_BUCKET_22997)
             {
