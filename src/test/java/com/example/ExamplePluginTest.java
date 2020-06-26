c5422944d80967026af9ad8340e6640317b8c8c8
Adam <Adam@sigterm.info>
2020-06-26 11:32:31 -0400

    Add death indicator plugin

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import info.sigterm.plugins.deathindicator.DeathIndicatorPlugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
@@ -7,7 +8,7 @@ public class ExamplePluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(DeathIndicatorPlugin.class);
 		RuneLite.main(args);
 	}
 }
