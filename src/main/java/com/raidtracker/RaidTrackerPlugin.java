bfb9fbc53996d35b3059c0d936f621638ae0d793
Cas van Mierlo <mail.casvm@gmail.com>
2020-07-02 18:38:50 +0200

    initial commit

--- src/main/java/com/raidtracker/RaidTrackerPlugin.java
+++ src/main/java/com/raidtracker/RaidTrackerPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.raidtracker;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class RaidTrackerPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private RaidTrackerConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	RaidTrackerConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(RaidTrackerConfig.class);
 	}
 }
--- src/test/java/com/raidtracker/RaidTrackerTest.java
+++ src/test/java/com/raidtracker/RaidTrackerTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.raidtracker;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class RaidTrackerTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(RaidTrackerPlugin.class);
 		RuneLite.main(args);
 	}
 }
