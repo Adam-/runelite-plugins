c30eb92b8a85c6bd64a36e048575444c353e0b10
Cas van Mierlo <mail.casvm@gmail.com>
2020-07-10 21:34:54 +0200

    verification metadata

---
+++ src/test/java/com/raidtracker/RunClientAndPlugin.java
@@ -0,0 +1,12 @@
+package com.raidtracker;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class RunClientAndPlugin {
+    public static void main(String[] args) throws Exception
+    {
+        ExternalPluginManager.loadBuiltin(RaidTrackerPlugin.class);
+        RuneLite.main(args);
+    }
+}
