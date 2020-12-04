c46070f714b2e797d68e2b6fc7e919ac96c9f1ce
Adam <Adam@sigterm.info>
2020-12-04 11:46:46 -0500

    Add /inv and /equip endpoints

--- src/main/java/info/sigterm/plugins/httpserver/HttpServerPlugin.java
+++ src/main/java/info/sigterm/plugins/httpserver/HttpServerPlugin.java
@@ -8,10 +8,17 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicReference;
 import javax.inject.Inject;
 import net.runelite.api.Client;
+import net.runelite.api.InventoryID;
+import net.runelite.api.Item;
+import net.runelite.api.ItemContainer;
 import net.runelite.api.Skill;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.http.api.RuneLiteAPI;
@@ -24,13 +31,18 @@ public class HttpServerPlugin extends Plugin
 	@Inject
 	private Client client;
 
+	@Inject
+	private ClientThread clientThread;
+
 	private HttpServer server;
 
 	@Override
 	protected void startUp() throws Exception
 	{
 		server = HttpServer.create(new InetSocketAddress(8080), 0);
-		server.createContext("/stats", new StatsHandler());
+		server.createContext("/stats", this::handleStats);
+		server.createContext("/inv", handlerForInv(InventoryID.INVENTORY));
+		server.createContext("/equip", handlerForInv(InventoryID.EQUIPMENT));
 		server.setExecutor(Executors.newSingleThreadExecutor());
 		server.start();
 	}
@@ -41,32 +53,84 @@ public class HttpServerPlugin extends Plugin
 		server.stop(1);
 	}
 
-	class StatsHandler implements HttpHandler
+	public void handleStats(HttpExchange exchange) throws IOException
 	{
-		@Override
-		public void handle(HttpExchange exchange) throws IOException
+		JsonArray skills = new JsonArray();
+		for (Skill skill : Skill.values())
 		{
-			JsonArray skills = new JsonArray();
-			for (Skill skill : Skill.values())
+			if (skill == Skill.OVERALL)
 			{
-				if (skill == Skill.OVERALL)
-				{
-					continue;
-				}
+				continue;
+			}
 
-				JsonObject object = new JsonObject();
-				object.addProperty("stat", skill.getName());
-				object.addProperty("level", client.getRealSkillLevel(skill));
-				object.addProperty("boostedLevel", client.getBoostedSkillLevel(skill));
-				object.addProperty("xp", client.getSkillExperience(skill));
-				skills.add(object);
+			JsonObject object = new JsonObject();
+			object.addProperty("stat", skill.getName());
+			object.addProperty("level", client.getRealSkillLevel(skill));
+			object.addProperty("boostedLevel", client.getBoostedSkillLevel(skill));
+			object.addProperty("xp", client.getSkillExperience(skill));
+			skills.add(object);
+		}
+
+		exchange.sendResponseHeaders(200, 0);
+		try (OutputStreamWriter out = new OutputStreamWriter(exchange.getResponseBody()))
+		{
+			RuneLiteAPI.GSON.toJson(skills, out);
+		}
+	}
+
+	private HttpHandler handlerForInv(InventoryID inventoryID)
+	{
+		return exchange -> {
+			Item[] items = invokeAndWait(() -> {
+				ItemContainer itemContainer = client.getItemContainer(inventoryID);
+				if (itemContainer != null)
+				{
+					return itemContainer.getItems();
+				}
+				return null;
+			});
+
+			if (items == null)
+			{
+				exchange.sendResponseHeaders(204, 0);
+				return;
 			}
 
 			exchange.sendResponseHeaders(200, 0);
 			try (OutputStreamWriter out = new OutputStreamWriter(exchange.getResponseBody()))
 			{
-				RuneLiteAPI.GSON.toJson(skills, out);
+				RuneLiteAPI.GSON.toJson(items, out);
 			}
+		};
+	}
+
+	private <T> T invokeAndWait(Callable<T> r)
+	{
+		try
+		{
+			AtomicReference<T> ref = new AtomicReference<>();
+			Semaphore semaphore = new Semaphore(0);
+			clientThread.invokeLater(() -> {
+				try
+				{
+
+					ref.set(r.call());
+				}
+				catch (Exception e)
+				{
+					throw new RuntimeException(e);
+				}
+				finally
+				{
+					semaphore.release();
+				}
+			});
+			semaphore.acquire();
+			return ref.get();
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException(e);
 		}
 	}
 }
