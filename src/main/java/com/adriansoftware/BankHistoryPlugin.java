757b7b33bdfc27ba24d57fd22a15456f75558d00
Adrian Elder <AdrianLeeElder@gmail.com>
2021-01-09 09:28:43 -0500

    Replace deprecated WidgetHiddenEvent
    Fixes #11

--- src/main/java/com/adriansoftware/BankHistoryPlugin.java
+++ src/main/java/com/adriansoftware/BankHistoryPlugin.java
@@ -28,13 +28,8 @@ import com.google.common.base.Strings;
 import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.ScriptCallbackEvent;
-import net.runelite.api.events.WidgetHiddenChanged;
-import net.runelite.api.events.WidgetLoaded;
-import net.runelite.api.widgets.Widget;
+import net.runelite.api.events.*;
 import net.runelite.api.widgets.WidgetID;
-import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -179,21 +174,10 @@ public class BankHistoryPlugin extends Plugin
 	}
 
 	@Subscribe
-	public void onWidgetHiddenChanged(WidgetHiddenChanged event)
-	{
-		Widget widget = event.getWidget();
-		int group = WidgetInfo.TO_GROUP(widget.getId());
-
-		//when bank interface is closed
-		//probably a better way to do this >.>
-		if (!widget.isHidden())
-		{
-			switch (group)
-			{
-				case WidgetID.RESIZABLE_VIEWPORT_OLD_SCHOOL_BOX_GROUP_ID:
-					bankHistoryPanel.setDatasetButton(false);
-					break;
-			}
+	public void onWidgetClosed(WidgetClosed event) {
+		if (event.getGroupId() == WidgetID.BANK_GROUP_ID) {
+			log.debug("onWidgetClosed: Bank closed");
+			bankHistoryPanel.setDatasetButton(false);
 		}
 	}
 
